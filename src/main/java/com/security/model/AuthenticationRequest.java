89e4cff68c3e72e58d64aa41d23112fa9e5072ac
Dinesh Kumar <dinesh.prasad@collinsongroup.com>
2023-02-06 23:15:25 +0530

    First Commit.

---
+++ src/main/java/com/security/model/AuthenticationRequest.java
@@ -0,0 +1,23 @@
+package com.security.model;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String username;
+	private String password;
+
+}
